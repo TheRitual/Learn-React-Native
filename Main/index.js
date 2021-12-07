520415309356a341b18562fc01dceb59ee815aab
Ritual <dev@theritual.eu>
2021-12-07 07:07:35 +0100

    added states that contain entered data and validate them

--- Main/index.js
+++ Main/index.js
@@ -4,13 +4,42 @@ import { Button, TextInput, View } from 'react-native';
 import styles from "./styles";
 
 const Main = () => {
-    const [buttonText, setButtonText] = useState("Who do I love?");
+    const minLength = 5;
+    const [enteredGoal, setEnteredGoal] = useState("");
+    const [courseGoals, setCourseGoals] = useState([]);
     
+
+    const addGoal = () => {
+        setCourseGoals(courseGoals =>
+            [
+                ...courseGoals,
+                enteredGoal,
+            ]
+        );
+    }
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    }
+
+    const addGoalHandler = () => {
+        if(enteredGoal.length > minLength) {
+            addGoal();
+            setEnteredGoal("");
+            console.log(courseGoals);
+        } else {
+            const msg = "Too short Goal:\nJust " + enteredGoal.length + " char(s)\nMinimum: " + minLength;
+            alert(msg);
+            console.log(msg);
+        }
+        
+    }
+
     return (
         <View style={styles.screen}>
             <View style={styles.topPanel}>
-                <TextInput placeholder="Course Goal" style={styles.textInput} />
-                <Button title="ADD" style={styles.button} />
+                <TextInput value={enteredGoal} placeholder="Course Goal" onChangeText={goalInputHandler} style={styles.textInput} />
+                <Button title="ADD" style={styles.button} onPress={() => addGoalHandler()} />
             </View>
             <View></View>
             <StatusBar />

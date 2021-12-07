5efd5044c4ec64fdde848cffec2177a19d59d004
Ritual <dev@theritual.eu>
2021-12-07 06:16:04 +0100

    created button and text input

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [buttonText, setButtonText] = useState("Who do I love?");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: "center" }}>
+        <TextInput placeholder="Course Goal" style={{ borderBottomColor: "#000", borderBottomWidth: 1, marginBottom: 10, color: "#000" }} />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
